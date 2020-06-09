f4b77a3659dcd2e4d292b450e7983148443e786a
SarmadTanveer <59303670+SarmadTanveer@users.noreply.github.com>
2020-06-09 14:32:49 -0400

    Add files via upload

---
+++ Monster-Rolodex/precache-manifest.dbd7acbc830e629a9935bc4db9414d72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81473991e4c1b622dc816862ae3c3ef5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "71bc10072e18502ff10b",
+    "url": "/static/css/main.081fb00e.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71bc10072e18502ff10b",
+    "url": "/static/js/main.e7ed72f4.chunk.js"
+  },
+  {
+    "revision": "4708b13b98379c58638d",
+    "url": "/static/js/runtime-main.03d56121.js"
+  }
+]);
